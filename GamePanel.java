673987eeaab3a71c2de207beda3cc117c857f617
Prajwal-A <93944298+Prajwal-A@users.noreply.github.com>
2023-08-10 19:57:21 +0530

    Add files via upload

---
+++ GamePanel.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+      GamePanel(){
+      }
+      
+      @Override
+      public void actionPerformed(ActionEvent e){
+      }
+}
